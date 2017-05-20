148c90592776dafe906bc56ef67947ced0009802
Felistas <felistas.ngumi@andela.com>
2017-05-20 14:53:41 +0300

    (Chore) Add bucketlist item schema

--- app/bucketlistapi/schema.py
+++ app/bucketlistapi/schema.py
@@ -20,9 +20,19 @@ class UserLoginSchema(Schema):
 
     password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required':'Password required'})
 
+class BucketListItemSchema(Schema):
+    """Schema class for adding bucketlist item"""
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True,error_messages={'required': 'Provide bucketlist item name'})
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime(dump_only=True)
+    done = fields.Boolean()
+    
 class BucketListSchema(Schema):
     """Schema class to validate bucketlist"""
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True,error_messages={'required': 'Provide bucketlist name'})
+    items = fields.Nested(BucketListItemSchema, dump_only=True, many=True)
+    created_by = fields.Integer(attribute='user.user_id', dump_only=True)
     date_created = fields.DateTime(dump_only=True)
     date_modified = fields.DateTime(dump_only=True)
