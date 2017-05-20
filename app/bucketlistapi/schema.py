d1ebdf361c8cecdd8ce71fd3eaeeff1e3c7ec66a
Felistas <felistas.ngumi@andela.com>
2017-05-20 13:56:49 +0300

    (Chore) Schema class for user login

--- app/bucketlistapi/schema.py
+++ app/bucketlistapi/schema.py
@@ -2,8 +2,7 @@ from marshmallow import Schema, fields,validate
 
 
 class UserResigrationSchema(Schema):
-
-
+"""Schema class to validate user during registration"""
     first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = 'First name required')
 
     last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message ='Last name required')
@@ -13,3 +12,9 @@ class UserResigrationSchema(Schema):
     password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = 'Password required')
 
     verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message= 'Password required')
+
+class UserLogin(Schema):
+    """Schema class to validate user during login"""
+    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = 'Email required')
+    
+    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = 'Password required')
