c1ac5d3cc2c727537cd7b402a28a40aec2a84ecb
Felistas <felistas.ngumi@andela.com>
2017-05-20 14:20:15 +0300

    [Fix] error messages in user login and registration schemas

--- app/bucketlistapi/schema.py
+++ app/bucketlistapi/schema.py
@@ -3,18 +3,18 @@ from marshmallow import Schema, fields,validate
 
 class UserResigrationSchema(Schema):
 """Schema class to validate user during registration"""
-    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = 'First name required')
+    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required':'First name required'})
 
-    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message ='Last name required')
+    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message ={'required': 'Last name required'})
 
-    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = 'Email required')
+    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required': 'Email required'})
 
-    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = 'Password required')
+    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = {'required': 'Password required'})
 
-    verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message= 'Password required')
+    verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message= {'required': 'Password required'})
 
-class UserLogin(Schema):
+class UserLoginSchema(Schema):
     """Schema class to validate user during login"""
-    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = 'Email required')
-    
-    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = 'Password required')
+    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required': 'Email required'})
+
+    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = {'required':'Password required'})
