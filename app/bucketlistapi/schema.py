c0e020386d854712593fa156b45bd39ee1455b4d
Felistas <felistas.ngumi@andela.com>
2017-05-20 14:40:52 +0300

    (Chore) Schema for bucketlist

--- app/bucketlistapi/schema.py
+++ app/bucketlistapi/schema.py
@@ -2,19 +2,27 @@ from marshmallow import Schema, fields,validate
 
 
 class UserResigrationSchema(Schema):
-"""Schema class to validate user during registration"""
-    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required':'First name required'})
 
-    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message ={'required': 'Last name required'})
+    """Schema class to validate user during registration"""
+    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages = {'required':'First name required'})
 
-    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required': 'Email required'})
+    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages ={'required': 'Last name required'})
 
-    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = {'required': 'Password required'})
+    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages = {'required': 'Email required'})
 
-    verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message= {'required': 'Password required'})
+    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required': 'Password required'})
+
+    verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required': 'Password required'})
 
 class UserLoginSchema(Schema):
     """Schema class to validate user during login"""
-    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_message = {'required': 'Email required'})
+    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages = {'required': 'Email required'})
 
-    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_message = {'required':'Password required'})
+    password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required':'Password required'})
+
+class BucketListSchema(Schema):
+    """Schema class to validate bucketlist"""
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True,error_messages={'required': 'Provide bucketlist name'})
+    date_created = fields.DateTime(dump_only=True)
+    date_modified = fields.DateTime(dump_only=True)
