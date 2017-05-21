2a07ddb9b8b3fbbcb80712e6934c5e5fb089d101
Felistas <felistas.ngumi@andela.com>
2017-05-21 11:58:32 +0300

    (Chore) verify if passwords match

--- app/bucketlistapi/resources.py
+++ app/bucketlistapi/resources.py
@@ -1,15 +1,13 @@
 
 from flask_restful import  Resource, abort
 from flask import request,jsonify
-from flask_httpauth import HTTPDigestAuth
-from flask_httpauth import HTTPBasicAuth
-from app.bucketlistapi.schema import (UserResigrationSchema,
+from app.bucketlistapi.schema import (UserRegistrationSchema,
                                       UserLoginSchema,
                                       BucketListItemSchema,
                                       BucketListSchema)
 
 
-user_register = UserResigrationSchema()
+user_register = UserRegistrationSchema()
 user_login = UserLoginSchema()
 bucket_list = BucketListSchema()
 bucket_list_item = BucketListItemSchema()
@@ -17,32 +15,36 @@ class UserRegister(Resource):
     """Register user"""
     def post(self):
         data = request.get_json()
-        print(data)
         if not data:
             response = {'error': 'No data provided'}
             return response, 400
         errors = user_register.validate(data)
         if errors:
-            return errors
-        email = data['email']
-        email = User.query.filter_by(email=email).first()
-        if email:
-            response = {'error': 'Email already in use'}
-            return response, 400
-        try:
-            #register user
-            new_user = User(first_name,last_name,email,password)
-            # new_user.first_name = first_name
-            # new_user.last_name = last_name
-            # username= first_name + last_name
-            # new_user.email = email
-            # new_user.password = password
-            db.session.add(new_user)
-            db.session.commit()
-            return {'message': '{} added successfully'.format(username)}, 201
-        except Exception as error:
-            db.session.rollback()
-            return {'error': '{} try again'.format(username)}, 400
+            return errors, 400
+        password = data['password']
+        verify_password = data['verify_password']
+        if password != verify_password:
+            return 'Passwords dont match'
+        return 'Nice'
+        # email = data['email']
+        # email = User.query.filter_by(email=email).first()
+        # if email:
+        #     response = {'error': 'Email already in use'}
+        #     return response, 400
+        # try:
+        #     #register user
+        #     new_user = User(first_name,last_name,email,password)
+        #     # new_user.first_name = first_name
+        #     # new_user.last_name = last_name
+        #     # username= first_name + last_name
+        #     # new_user.email = email
+        #     # new_user.password = password
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     return {'message': '{} added successfully'.format(username)}, 201
+        # except Exception as error:
+        #     db.session.rollback()
+        #     return {'error': '{} try again'.format(username)}, 400
 
 
 
--- app/bucketlistapi/schema.py
+++ app/bucketlistapi/schema.py
@@ -1,12 +1,12 @@
 from marshmallow import Schema, fields,validate
 
 
-class UserResigrationSchema(Schema):
+class UserRegistrationSchema(Schema):
 
     """Schema class to validate user during registration"""
-    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages = {'required':'First name required'})
-    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages ={'required': 'Last name required'})
-    email = fields.String(required = True, load_only=True, validate=[validate.Length(max=12)],error_messages = {'required': 'Email required'})
+    first_name = fields.String(required = True, load_only=True, validate=[validate.Length(min=3, max=12)],error_messages = {'required':'First name required'})
+    last_name = fields.String(required = True, load_only=True, validate=[validate.Length(min=3, max=12)],error_messages ={'required': 'Last name required'})
+    email = fields.Email(required = True, load_only=True, validate=[validate.Length(min=20)],error_messages = {'required': 'Email required'})
     password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required': 'Password required'})
     verify_password = fields.String(required = True, load_only=True, validate=[validate.Length(min=6)],error_messages = {'required': 'Password required'})
 
