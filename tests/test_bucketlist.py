9ee18b8606fab44795f4171122e54b87d7894bbc
Felistas <felistas.ngumi@andela.com>
2017-05-26 15:43:31 +0300

    [Fix] fix tests and implemented feedback

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -2,13 +2,16 @@ import unittest
 from flask import json
 from app import create_app
 from instance.config import app_config
-from app.models import User, BucketList, BucketListItems, db
+# from app.models import User, BucketList, BucketListItems, db
+
 
 
 class BucketlistTestCase(unittest.TestCase):
     """This class represents the bucketlist test cases"""
     def setUp(self):
         self.app = create_app('testing')
+        with self.app.app_context():
+            from app.models import db, User, BucketList, BucketListItems
         #gets the current state
         self.app_context = self.app.app_context()
         self.app_context.push()
@@ -30,8 +33,10 @@ class BucketlistTestCase(unittest.TestCase):
         }
         new_user = self.create_user()
         logged_in_user = self.login_user()
-        self.header={"Content-Type": "application/json", 'Accept': 'application/json'}
-        self.header['Authorization'] = 'Bearer ' + json.loads(logged_in_user.get_data(as_text=True))['token']
+        self.header={"Content-Type": "application/json",
+                     'Accept': 'application/json'}
+        self.header['Authorization'] = 'Bearer ' + json.loads(
+            logged_in_user.get_data(as_text=True))['token']
         self.new_bucketlist = {
             'name': 'Learn German'
         }
@@ -41,16 +46,26 @@ class BucketlistTestCase(unittest.TestCase):
         }
 
     def create_user(self):
-        created_user = self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
+        created_user = self.client.post(
+            '/api/v1/auth/register',data=json.dumps(self.user),
+            headers={"Content-Type": "application/json",
+                     'Accept': 'application/json'})
         return created_user
     def login_user(self):
-        current_user = self.client.post('/api/v1/auth/login', data=json.dumps(self.login),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
+        current_user = self.client.post(
+            '/api/v1/auth/login', data=json.dumps(self.login),
+            headers={"Content-Type": "application/json",
+                     'Accept': 'application/json'})
         return current_user
     def new_bucket_list(self):
-        new_bucket = self.client.post('/api/v1/bucketlists/', headers=self.header,data=json.dumps(self.new_bucketlist))
+        new_bucket = self.client.post('/api/v1/bucketlists/',
+                                      headers=self.header,
+                                      data=json.dumps(self.new_bucketlist))
         return new_bucket
 
     def tearDown(self):
+        with self.app.app_context():
+            from app.models import db, User, BucketList, BucketListItems
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
@@ -58,12 +73,14 @@ class BucketlistTestCase(unittest.TestCase):
     def test_create_bucketlist_successfully(self):
         """Test API can create a bucketlist succesfully"""
         new_bucket = self.new_bucket_list()
-        self.assertEqual(json.loads(new_bucket.get_data(as_text=True))['status'], 201)
+        self.assertEqual(json.loads(new_bucket.get_data(as_text=True))
+                         ['status'], 201)
     def test_cannot_create_existing_bucketlist(self):
         """Test API cannot create existing bucketlist"""
         first_bucketlist=self.new_bucket_list()
         second_bucketlist=self.new_bucket_list()
-        self.assertEqual(json.loads(second_bucketlist.get_data(as_text=True))['status'], 409)
+        self.assertEqual(json.loads(second_bucketlist.get_data(as_text=True))
+                         ['status'], 409)
 
     def test_delete_bucketlist_successfully(self):
         """Test API can delete a bucketlist successfully"""
@@ -81,13 +98,17 @@ class BucketlistTestCase(unittest.TestCase):
     def test_create_bucketlist_item_successfully(self):
         """Test API can create a bucketlist item successfully"""
         self.new_bucket_list()
-        response = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
-        self.assertEqual(json.loads(response.get_data(as_text=True))['status'], 201)
+        response = self.client.post('/api/v1/bucketlists/1/items/',
+                                    headers=self.header,
+                                    data=json.dumps(self.new_bucketlist_item))
+        self.assertEqual(json.loads(response.get_data(as_text=True))['status'],
+                        201)
 
     def test_cannot_create_bucketlist_if_unauthorized(self):
         """Test cannot create bucketlist if unauthorized"""
         #add unauthorized user
-        response = self.client.post('/api/v1/bucketlists/', data=json.dumps(self.new_bucketlist))
+        response = self.client.post('/api/v1/bucketlists/',
+                                    data=json.dumps(self.new_bucketlist))
         self.assertEqual(response.status_code, 401)
 
     def test_cannot_list_all_bucketlists_if_unauthorized(self):
@@ -105,7 +126,8 @@ class BucketlistTestCase(unittest.TestCase):
     def test_cannot_create_bucketlist_item_if_unauthorized(self):
         """Test API cannot create a bucketlist item if unauthorized"""
         #add unauthorized user
-        response = self.client.post('/api/v1/bucketlists/1/items/',data=json.dumps(self.new_bucketlist_item))
+        response = self.client.post('/api/v1/bucketlists/1/items/',
+                                    data=json.dumps(self.new_bucketlist_item))
         self.assertEqual(response.status_code, 401)
 
     def test_edit_bucketlist(self):
@@ -114,26 +136,37 @@ class BucketlistTestCase(unittest.TestCase):
         edit_bucketlist = {
             'name': 'Learn French'
         }
-        response = self.client.put('/api/v1/bucketlists/1',headers=self.header,data=json.dumps(edit_bucketlist))
+        response = self.client.put('/api/v1/bucketlists/1',
+                                   headers=self.header,
+                                   data=json.dumps(edit_bucketlist))
         self.assertEqual(response.status_code, 200)
 
     def test_delete_item_successfully(self):
         """Test one can delete an item successfully"""
         self.new_bucket_list()
-        response = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
-        response = self.client.delete('/api/v1/bucketlists/1/items/1',headers=self.header)
+        response = self.client.post('/api/v1/bucketlists/1/items/',
+                                    headers=self.header,
+                                    data=json.dumps(self.new_bucketlist_item))
+        response = self.client.delete('/api/v1/bucketlists/1/items/1',
+                                      headers=self.header)
         self.assertEqual(response.status_code, 200)
     def test_edit_bucketlist_item_successfully(self):
         """Assert updating item successfully"""
         update_item = {"name":"ride"}
         self.new_bucket_list()
-        response = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
-        response = self.client.put('/api/v1/bucketlists/1/items/1',headers=self.header, data=json.dumps(update_item))
+        response = self.client.post('/api/v1/bucketlists/1/items/',
+                                    headers=self.header,
+                                    data=json.dumps(self.new_bucketlist_item))
+        response = self.client.put('/api/v1/bucketlists/1/items/1',
+                                   headers=self.header,
+                                   data=json.dumps(update_item))
         self.assertEqual(response.status_code, 200)
     def test_cannot_get_bucketlist_item_if_unauthorized(self):
         """Assert that cannot list item if unauthorized"""
         self.new_bucket_list()
-        response = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
+        response = self.client.post('/api/v1/bucketlists/1/items/',
+                                    headers=self.header,
+                                    data=json.dumps(self.new_bucketlist_item))
         response = self.client.get('/api/v1/bucketlists/1/items/1')
         self.assertEqual(response.status_code, 401)
     def test_cannot_get_item_if_non_existent(self):
@@ -143,18 +176,27 @@ class BucketlistTestCase(unittest.TestCase):
     def test_cannot_create_already_existing_item(self):
         """Assert that one cannot create an aleady existing item"""
         self.new_bucket_list()
-        self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
-        existing_item = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
-        self.assertEqual(json.loads(existing_item.get_data(as_text=True))['status'], 409)
+        self.client.post('/api/v1/bucketlists/1/items/',
+                         headers=self.header,
+                         data=json.dumps(self.new_bucketlist_item))
+        existing_item = self.client.post('/api/v1/bucketlists/1/items/',
+                                         headers=self.header,
+                                         data=json.dumps(
+                                             self.new_bucketlist_item))
+        self.assertEqual(
+            json.loads(existing_item.get_data(as_text=True))['status'], 409)
     def test_cannot_create_item_if_name_not_provided(self):
-        """Assert that one cannot create bucketlist item if no items are provided"""
+        """Assert cannot create bucketlist item if no items are provided"""
         self.new_bucket_list()
         no_data = {}
-        item = self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(no_data))
+        item = self.client.post('/api/v1/bucketlists/1/items/',
+                                headers=self.header, data=json.dumps(no_data))
         self.assertEqual(json.loads(item.get_data(as_text=True))['status'], 400)
     def test_cannot_get_item_for_wrong_url(self):
         """Asserts that one cannot get an item for the wrong url"""
         self.new_bucket_list()
-        self.client.post('/api/v1/bucketlists/1/items/',headers=self.header, data=json.dumps(self.new_bucketlist_item))
+        self.client.post('/api/v1/bucketlists/1/items/',
+                         headers=self.header,
+                         data=json.dumps(self.new_bucketlist_item))
         response = self.client.get('/api/v1/bucketlists/1/item/12')
         self.assertEqual(response.status_code, 404)
--- tests/test_user.py
+++ tests/test_user.py
@@ -71,59 +71,120 @@ class UserTest(unittest.TestCase):
 
     def test_create_user_successfully(self):
         """Asserts that a user can be created successfully"""
-        response = self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/register',
+                                    data=json.dumps(self.user),
+                                    headers={
+                                        "Content-Type": "application/json",
+                                        'Accept': 'application/json'})
         self.assertEquals(response.status_code, 200)
     def test_login_successful(self):
         """Asserts a user can login successfully"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.login),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={
+                             "Content-Type": "application/json",
+                             'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(self.login),
+                                    headers={
+                                        "Content-Type": "application/json",
+                                         'Accept': 'application/json'})
         self.assertEqual(response.status_code, 200)
 
     def test_login_with_invalid_password(self):
         """Asserts user cannot login with invalid password"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.invalid_password),headers = {"Content-Type": "application/json"})
+        self.client.post('/api/v1/auth/register',
+                         data=json.dumps(self.user),
+                         headers={
+                             "Content-Type": "application/json",
+                             'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(self.invalid_password),
+                                    headers = {
+                                        "Content-Type": "application/json"})
         self.assertEqual(response.status_code, 400)
     def test_login_with_no_credentials(self):
         """Asserts user cannot login with no credentials"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.invalid_password),headers = {"Content-Type": "application/json"})
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(self.invalid_password),
+                                    headers = {
+                                        "Content-Type": "application/json"})
         self.assertEqual(response.status_code, 400)
 
     def test_login_with_no_username(self):
         """Asserts user cannot login with no username"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login/', data=json.dumps(self.login_with_no_username),headers = {"Content-Type": "application/json"})
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login/',
+                                    data=json.dumps(
+                                        self.login_with_no_username),
+                                    headers = {
+                                        "Content-Type": "application/json"})
         self.assertEqual(response.status_code, 404)
 
     def test_login_with_no_password(self):
         """Asserts user cannot login with no password"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.login_with_no_password),headers = {"Content-Type": "application/json"})
+        self.client.post('/api/v1/auth/register',
+                         data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(
+                                        self.login_with_no_password),
+                                    headers = {
+                                        "Content-Type": "application/json"})
         self.assertEqual(response.status_code, 400)
 
     def test_create_user_with_password_mismatch(self):
-        """Asserts that a user cannot be created successfully with wrong passwords"""
-        response = self.client.post('/api/v1/auth/register',data=json.dumps(self.wrong_password),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        self.assertEquals(json.loads(response.get_data(as_text=True))['status'], 400)
+        """Asserts user cannot be created successfully with wrong passwords"""
+        response = self.client.post('/api/v1/auth/register',
+                                    data=json.dumps(self.wrong_password),
+                                    headers={"Content-Type": "application/json",
+                                             'Accept': 'application/json'})
+        self.assertEquals(json.loads(
+            response.get_data(as_text=True))['status'], 400)
     #
     def test_register_with_no_data(self):
         """Asserts that a user cannot register with no data"""
-        response = self.client.post('/api/v1/auth/register',data=json.dumps(self.no_data),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        self.assertEquals(json.loads(response.get_data(as_text=True))['status'], 400)
+        response = self.client.post('/api/v1/auth/register',
+                                    data=json.dumps(self.no_data),
+                                    headers={"Content-Type": "application/json",
+                                             'Accept': 'application/json'})
+        self.assertEquals(json.loads(
+            response.get_data(as_text=True))['status'], 400)
 
     def test_cannot_register_existing_user(self):
         """Asserts cannot register an already existing user"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        self.assertEquals(json.loads(response.get_data(as_text=True))['status'], 409)
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/register',
+                                    data=json.dumps(self.user),
+                                    headers={"Content-Type": "application/json",
+                                             'Accept': 'application/json'})
+        self.assertEquals(json.loads(
+            response.get_data(as_text=True))['status'], 409)
     def test_cannot_login_with_no_data(self):
         """Asserts user cannot login if data is not provided"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.login_no_credentials),headers = {"Content-Type": "application/json"})
-        self.assertEquals(json.loads(response.get_data(as_text=True))['status'], 400)
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(self.login_no_credentials),
+                                    headers = {
+                                        "Content-Type": "application/json"})
+        self.assertEquals(json.loads(
+            response.get_data(as_text=True))['status'], 400)
     def test_cannot_login_with_invalid_email(self):
         """Asserts that a user cannot login with invalid email"""
-        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),headers={"Content-Type": "application/json", 'Accept': 'application/json'})
-        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.login_wrong_email),headers = {"Content-Type": "application/json"})
+        self.client.post('/api/v1/auth/register',data=json.dumps(self.user),
+                         headers={"Content-Type": "application/json",
+                                  'Accept': 'application/json'})
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(self.login_wrong_email),
+                                    headers = {
+                                        "Content-Type": "application/json"})
         self.assertEqual(response.status_code, 400)
